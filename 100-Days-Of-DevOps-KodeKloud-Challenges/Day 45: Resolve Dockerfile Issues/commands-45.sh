c4fb943eb47583994cca39b233e7101a99a19aa6
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-15 17:31:03 +0200

    Update commands-45.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 45: Resolve Dockerfile Issues/commands-45.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 45: Resolve Dockerfile Issues/commands-45.sh
@@ -1,1 +1,33 @@
+# connect to server 1 
+sudo ssh tony@stapp01
+
+# To fix the issue : 
+
+cd /opt/docker ; ls 
+# [root@stapp01 docker]# ls
+# Dockerfile  certs  html
+cat /opt/docker/Dockerfile
+# FROM httpd:2.4.43
+# RUN sed -i "s/Listen 80/Listen 8080/g" /usr/local/apache2/conf/httpd.conf
+# RUN sed -i '/LoadModule\ ssl_module modules\/mod_ssl.so/s/^#//g' conf/httpd.conf
+# RUN sed -i '/LoadModule\ socache_shmcb_module modules\/mod_socache_shmcb.so/s/^#//g' conf/httpd.conf
+# RUN sed -i '/Include\ conf\/extra\/httpd-ssl.conf/s/^#//g' conf/httpd.conf
+# COPY /server.crt /usr/local/apache2/conf/server.crt
+# COPY /server.key /usr/local/apache2/conf/server.key
+
+# Right version is : 
+FROM httpd:2.4.43
+
+RUN sed -i "s/Listen 80/Listen 8080/g" /usr/local/apache2/conf/httpd.conf
+
+RUN sed -i '/LoadModule\ ssl_module modules\/mod_ssl.so/s/^#//g' /usr/local/apache2/conf/httpd.conf
+RUN sed -i '/LoadModule\ socache_shmcb_module modules\/mod_socache_shmcb.so/s/^#//g' /usr/local/apache2/conf/httpd.conf
+RUN sed -i '/Include\ conf\/extra\/httpd-ssl.conf/s/^#//g' /usr/local/apache2/conf/httpd.conf
+
+COPY server.crt /usr/local/apache2/conf/server.crt
+COPY server.key /usr/local/apache2/conf/server.key
+
+# to verify 
+cd /opt/docker
+docker build -t custom-httpd .
 
