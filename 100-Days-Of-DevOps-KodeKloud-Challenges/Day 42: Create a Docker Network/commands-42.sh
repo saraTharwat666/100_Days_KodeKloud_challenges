bdd129fb4248a0b091e74ca74e164a76cc56fc58
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-15 16:43:10 +0200

    Update commands-42.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 42: Create a Docker Network/commands-42.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 42: Create a Docker Network/commands-42.sh
@@ -1,1 +1,11 @@
+# connect to Server 3
+sudo ssh banner@stapp03
 
+# Create a docker network named as official on App Server 3 in Stratos DC.
+
+docker network create \
+  --driver macvlan \
+  --subnet=192.168.0.0/24 \
+  --ip-range=192.168.0.0/24 \
+  -o parent=eth0 \
+  official
