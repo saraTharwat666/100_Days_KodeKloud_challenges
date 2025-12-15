cddce08554bf5db82e8f0d9ecb7c527e4fb8d57c
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-15 17:21:59 +0200

    Update commands-44.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 44: Write a Docker Compose File/commands-44.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 44: Write a Docker Compose File/commands-44.sh
@@ -1,1 +1,19 @@
+# connect to server 2
+sudo ssh steve@stapp02
+# Create a docker-compose file at /opt/docker/
+touch /opt/docker/docker-compose.yml ; vi docker-compose.yml 
+
+# The Content of docker-compose file 
+version: "3.8"
+services:
+  web:
+    image: httpd:latest
+    container_name: httpd
+    ports:
+      - "3003:80"
+    volumes:
+      - /opt/security:/usr/local/apache2/htdocs
+
+# to run the container
+docker compose -f /opt/docker/docker-compose.yml up -d
 
