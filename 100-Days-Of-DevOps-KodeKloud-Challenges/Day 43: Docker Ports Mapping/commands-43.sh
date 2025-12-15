83a21dce2e555906a48cec765b9a6a06610e81e3
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-15 16:50:05 +0200

    Update commands-43.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 43: Docker Ports Mapping/commands-43.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 43: Docker Ports Mapping/commands-43.sh
@@ -1,1 +1,5 @@
+# connect to server 2
+sudo ssh steve@stapp02
 
+#  Pull nginx:alpine-perl docker image on Application Server 2 && Create a container named games using the image you pulled.
+docker run -d --name games -p 8083:80 nginx:alpine-perl
