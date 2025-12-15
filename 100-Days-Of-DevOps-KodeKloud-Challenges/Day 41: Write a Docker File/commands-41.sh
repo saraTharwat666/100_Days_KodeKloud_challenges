12d670207af423f17682615e4a6b38cc971bea00
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-15 16:20:44 +0200

    Update commands-41.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 41: Write a Docker File/commands-41.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 41: Write a Docker File/commands-41.sh
@@ -1,1 +1,17 @@
+#connect to app server 1
+sudo ssh tony@stapp01
+#  create a docker file /opt/docker/Dockerfile
+touch /opt/docker/Dockerfile
+vi /opt/docker/Dockerfile
 
+# this is a content of Docker file to install apache 
+
+# FROM ubuntu:24.04
+
+# RUN apt-get update && \
+#     apt-get install -y apache2 && \
+#     sed -i 's/Listen 80/Listen 5001/' /etc/apache2/ports.conf
+
+# EXPOSE 5001
+
+# CMD ["apachectl", "-D", "FOREGROUND"]
