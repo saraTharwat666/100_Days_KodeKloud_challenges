abfa22759199275d9d36c6fbc7949ac4a9556f4b
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-15 18:51:21 +0200

    Update commands-47.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 47: Docker Python App/commands-47.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 47: Docker Python App/commands-47.sh
@@ -1,1 +1,25 @@
+# connect to server 1
+sudo ssh tony@stapp01
+# create Dockerfile and add the content into it : 
+vi /python_app/Dockerfile
+# content od Dockerfile
+
+FROM python:3.12-slim
+
+WORKDIR /app
+
+COPY src/requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY src/ .
+
+EXPOSE 8087
+
+CMD ["python", "server.py"]
+
+
+# To build The image : 
+cd /python_app
+docker build -t nautilus/python-app .
 
