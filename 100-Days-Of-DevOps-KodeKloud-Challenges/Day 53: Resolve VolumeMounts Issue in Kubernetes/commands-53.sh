386e440ba15af7fe1d3d9b5c2afa9c4ddd1c35d3
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-16 19:01:59 +0200

    Update commands-53.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 53: Resolve VolumeMounts Issue in Kubernetes/commands-53.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 53: Resolve VolumeMounts Issue in Kubernetes/commands-53.sh
@@ -1,1 +1,11 @@
+# Step 1: Check the Pod Status
+kubectl get pods
 
+# Step 2: Verify the Document Root
+kubectl exec -it nginx-phpfpm -c nginx-container -- ls -la /usr/share/nginx/html
+
+# Step 3: Copy the index.php File to the Pod
+kubectl cp /home/thor/index.php nginx-phpfpm:/usr/share/nginx/html/index.php -c nginx-container
+
+# Step 4: Verify the Website
+kubectl get pods
