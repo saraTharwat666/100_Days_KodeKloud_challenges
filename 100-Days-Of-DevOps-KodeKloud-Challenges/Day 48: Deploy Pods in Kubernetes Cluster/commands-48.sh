bcbdad41916035d52faa1ce217227c394933901d
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-16 17:26:38 +0200

    Update commands-48.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 48: Deploy Pods in Kubernetes Cluster/commands-48.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 48: Deploy Pods in Kubernetes Cluster/commands-48.sh
@@ -1,1 +1,21 @@
+# create a yaml file 
+vi pod-httpd.yml
 
+
+# content of yaml file : 
+apiVersion: v1
+kind: Pod
+metadata:
+  name: pod-httpd
+  labels:
+    app: httpd_app
+spec:
+  containers:
+    - name: httpd-container
+      image: httpd:latest
+
+# To apply the file 
+kubectl apply -f pod-httpd.yml
+
+# To verify :
+kubectl get pods
