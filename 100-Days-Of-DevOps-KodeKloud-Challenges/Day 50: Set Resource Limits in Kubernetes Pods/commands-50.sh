1b053df4e4486370493947de21d7d3919a8ba61b
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-16 17:47:31 +0200

    Update commands-50.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 50: Set Resource Limits in Kubernetes Pods/commands-50.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 50: Set Resource Limits in Kubernetes Pods/commands-50.sh
@@ -1,1 +1,22 @@
+# create yaml file 
+vi httpd-pod.yml 
 
+# content of yaml file:
+apiVersion: v1
+kind: Pod
+metadata:
+  name: httpd-pod
+spec:
+  containers:
+  - name: httpd-container
+    image: httpd:latest
+    resources:
+      requests:
+        memory: "15Mi"
+        cpu: "100m"
+      limits:
+        memory: "20Mi"
+        cpu: "100m"
+
+# to apply this :
+kubectl apply -f httpd-pod.yml
