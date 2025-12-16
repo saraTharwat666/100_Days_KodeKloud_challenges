34195dda7cdac2582ec65f0c65e09f34008fba2d
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-16 17:36:41 +0200

    Update commands-49.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 49: Deploy Applications with Kubernetes Deployments/commands-49.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 49: Deploy Applications with Kubernetes Deployments/commands-49.sh
@@ -1,1 +1,29 @@
+# create yaml file 
+vi nginx-deployment.yml 
+
+
+# content of file 
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:latest
+        ports:
+        - containerPort: 80
+# to apply file 
+kubectl apply -f nginx-deployment.yml
+# to verify : 
+kubectl get pods
 
