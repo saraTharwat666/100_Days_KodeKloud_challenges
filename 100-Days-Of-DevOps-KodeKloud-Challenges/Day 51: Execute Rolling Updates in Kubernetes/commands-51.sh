59a84d97b4bbdac5964e240d1fccccf0f774a309
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-16 18:06:42 +0200

    Update commands-51.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 51: Execute Rolling Updates in Kubernetes/commands-51.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 51: Execute Rolling Updates in Kubernetes/commands-51.sh
@@ -1,1 +1,9 @@
+#  to know container name [kubectl get deployment nginx-deployment -o yaml]
+# Execute a rolling update for this application [deployment/nginx-deployment] ==> [nginx=nginx:1.19]
+kubectl set image deployment/nginx-deployment nginx-container=nginx:1.19
+
+# verify the changes : 
+kubectl rollout status deployment/nginx-deployment
+kubectl get pods -o wide
+
 
