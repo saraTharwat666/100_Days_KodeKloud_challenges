0053acc123f44ca10fdd338d8a707106fe7f0afd
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-16 18:14:19 +0200

    Update commands-52.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 52: Revert Deployment to Previous Version in Kubernetes/commands-52.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 52: Revert Deployment to Previous Version in Kubernetes/commands-52.sh
@@ -1,1 +1,5 @@
+# initiate a rollback to the previous revision.
+kubectl rollout undo deployment/nginx-deployment
 
+# to verify rollback is done:
+kubectl rollout status deployment/nginx-deployment  #deployment "nginx-deployment" successfully rolled out
