5399072967719c324e1d1eb8b5fadb7437f034aa
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-19 21:15:49 +0200

    Update commands-55.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 55: Kubernetes Sidecar Containers/commands-55.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 55: Kubernetes Sidecar Containers/commands-55.sh
@@ -1,1 +1,32 @@
+# create yaml file to create pod
+vi webserver-pod.yaml
+
+apiVersion: v1
+kind: Pod
+metadata:
+  name: webserver
+spec:
+  volumes:
+  - name: shared-logs
+    emptyDir: {}
+  containers:
+  - name: nginx-container
+    image: nginx:latest
+    volumeMounts:
+    - name: shared-logs
+      mountPath: /var/log/nginx
+  - name: sidecar-container
+    image: ubuntu:latest
+    command: ["sh","-c","while true; do cat /var/log/nginx/access.log /var/log/nginx/error.log; sleep 30; done"]
+    volumeMounts:
+    - name: shared-logs
+      mountPath: /var/log/nginx
+# apply yaml file
+kubectl apply -f webserver-pod.yaml
+
+# Verify Logs Sharing
+kubectl exec -it webserver -c sidecar-container -- ls /var/log/nginx
+kubectl exec -it webserver -c sidecar-container -- cat /var/log/nginx/access.log
+kubectl exec -it webserver -c sidecar-container -- cat /var/log/nginx/error.log
+
 
