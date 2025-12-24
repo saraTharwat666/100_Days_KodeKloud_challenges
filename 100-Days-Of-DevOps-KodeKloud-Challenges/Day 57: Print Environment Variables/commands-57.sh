b1a0d7f9e2a949109e0c57a9fcfcb5cb60b3bf5c
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-24 16:56:50 +0200

    Update commands-57.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 57: Print Environment Variables/commands-57.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 57: Print Environment Variables/commands-57.sh
@@ -1,1 +1,27 @@
-
+# create yaml file 
+vi print-envars-greeting.yaml
+# its content
+apiVersion: v1
+kind: Pod
+metadata:
+  name: print-envars-greeting
+spec:
+  restartPolicy: Never
+  containers:
+  - name: print-env-container
+    image: bash
+    command:
+      - /bin/sh
+      - -c
+      - 'echo "$(GREETING) $(COMPANY) $(GROUP)"'
+    env:
+    - name: GREETING
+      value: "Welcome to"
+    - name: COMPANY
+      value: "Stratos"
+    - name: GROUP
+      value: "Industries"
+# apply the yaml file
+kubectl apply -f print-envars-greeting.yaml
+# for testing
+kubectl logs -f print-envars-greeting  # Welcome to Stratos Industries
