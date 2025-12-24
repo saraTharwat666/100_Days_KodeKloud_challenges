d7dfcbf8ed86b6e49b607b1ff3499febbd7a3284
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-24 17:05:02 +0200

    Update commands-58.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 58: Deploy Grafana on Kubernetes Cluster/commands-58.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 58: Deploy Grafana on Kubernetes Cluster/commands-58.sh
@@ -1,1 +1,45 @@
+# create yaml file
+vi grafana-deployment.yaml
+# its content
+
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: grafana-deployment-datacenter
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: grafana
+  template:
+    metadata:
+      labels:
+        app: grafana
+    spec:
+      containers:
+      - name: grafana-container
+        image: grafana/grafana:latest
+        ports:
+        - containerPort: 3000
+
+# create service file
+vi grafana-service.yaml
+
+# its content
+apiVersion: v1
+kind: Service
+metadata:
+  name: grafana-service
+spec:
+  type: NodePort
+  selector:
+    app: grafana
+  ports:
+  - port: 3000
+    targetPort: 3000
+    nodePort: 32000
+
+# apply two files 
+kubectl apply -f grafana-deployment.yaml
+kubectl apply -f grafana-service.yaml
 
