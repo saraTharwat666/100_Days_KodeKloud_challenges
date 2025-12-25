0676788afdb0e3f2135f05d63ca74d580d17c1a0
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-25 19:52:11 +0200

    Update commands-64.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 64: Fix Python App Deployed on Kubernetes Cluster/commands-64.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 64: Fix Python App Deployed on Kubernetes Cluster/commands-64.sh
@@ -1,1 +1,10 @@
+# check  pods :
+kubectl get pods
+kubectl get pods -o wide
 
+# check the services : 
+
+kubectl get svc
+kubectl describe svc <service name>
+# edit the port to match deployment eith service 
+# edit the name of image
