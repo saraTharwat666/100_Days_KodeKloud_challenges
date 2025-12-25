1d2da0e4d14cfe8d659af58182182b6c7b2fcf4b
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-25 17:16:00 +0200

    Update commands-59.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 59: Troubleshoot Deployment issues in Kubernetes/commands-59.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 59: Troubleshoot Deployment issues in Kubernetes/commands-59.sh
@@ -1,1 +1,18 @@
+ # Edit the Redis Deployment
+ kubectl edit deployment redis-deployment
+ 
+# edit this file by checking it and correct the mistake:
+# - configMap:
+#           defaultMode: 420
+#           name: redis-cofig   ==> config instead cofig
+#         name: config
+
+# and 
+  # containers:
+  #     - image: redis:alpin  ===> alpine instead alpin
+  #       imagePullPolicy: IfNotPresent
+
+  
+kubectl get deployments -o wide 
+
 
