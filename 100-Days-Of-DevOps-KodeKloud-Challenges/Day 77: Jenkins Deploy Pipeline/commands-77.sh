ad72edc569e9de0c51aef8af14eace39505925bf
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-30 15:49:46 +0200

    Update commands-77.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 77: Jenkins Deploy Pipeline/commands-77.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 77: Jenkins Deploy Pipeline/commands-77.sh
@@ -1,1 +1,24 @@
+3️⃣ Configure Pipeline Script
 
+Pipeline Definition
+
+'''
+Select:
+Pipeline script
+
+Pipeline Script
+pipeline {
+    agent { label 'ststor01' }
+
+    stages {
+        stage('Deploy') {
+            steps {
+                sh '''
+                cd /var/www/html
+                git pull origin main
+                '''
+            }
+        }
+    }
+} 
+'''
