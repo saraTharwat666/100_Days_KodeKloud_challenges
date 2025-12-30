269040a614babe72bbca78ffabac1d9fa16e24b1
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2025-12-30 16:22:28 +0200

    Update commands-78.sh

--- 100-Days-Of-DevOps-KodeKloud-Challenges/Day 78: Jenkins Conditional Pipeline/commands-78.sh
+++ 100-Days-Of-DevOps-KodeKloud-Challenges/Day 78: Jenkins Conditional Pipeline/commands-78.sh
@@ -1,1 +1,29 @@
 
+# pipeline Script 
+
+
+pipeline {
+    agent { label 'ststor01' }
+
+    parameters {
+        string(name: 'BRANCH', defaultValue: 'master', description: 'Branch to deploy (master or feature)')
+    }
+
+    stages {
+        stage('Deploy') {
+            steps {
+                sh '''
+                cd /var/www/html
+
+                git init
+                git remote remove origin || true
+                git remote add origin http://git.stratos.xfusioncorp.com/sarah/web_app.git
+
+                git fetch origin
+                git checkout ${BRANCH}
+                git pull origin ${BRANCH}
+                '''
+            }
+        }
+    }
+}
